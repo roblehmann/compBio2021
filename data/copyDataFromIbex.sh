6d0e9e92725095da936befb9218bd32e8fff0a18
lehmanr <robert.lehman@kaust.edu.sa>
2020-02-24 14:21:27 +0300

    move qc scripts

--- data/copyDataFromIbex.sh
+++ data/copyDataFromIbex.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+inPath='/ibex/scratch/projects/c2024/labcourse2020_genomics_data'
+
+cp ${inPath}/Practical_Manual.pdf .
+
 # copy gene annotation, protein sequences, genome seq. for comparison of our results
 echo "copying reference genome files..."
-cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/GCF_000005845.2_ASM584v2* .
+cp ${inPath}/GCF_000005845.2_ASM584v2* .
 
 # get busco gene set database to assess completeness of our assmbly candidates
 echo 'copying busco files...'
-cp -r /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/enterobacteriales_odb9 .
+cp -r ${inPath}/enterobacteriales_odb9 .
 
 # copy small version of kraken2 database
 echo "copying Kraken2 files..."
-cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/*k2d .
+cp ${inPath}/*k2d .
 
