0c18078b017ffb88f68d8df1b2a3ffbf32de07ff
lehmanr <robert.lehman@kaust.edu.sa>
2020-02-24 11:52:47 +0300

    copy kraken files last since they are largest

--- data/copyDataFromIbex.sh
+++ data/copyDataFromIbex.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# copy small version of kraken2 database
-echo "copying Kraken files..."
-cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/*k2d .
-
 # copy gene annotation, protein sequences, genome seq. for comparison of our results
 echo "copying reference genome files..."
 cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/GCF_000005845.2_ASM584v2* .
@@ -11,3 +7,8 @@ cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/GCF_000005845.2_AS
 # get busco gene set database to assess completeness of our assmbly candidates
 echo 'copying busco files...'
 cp -r /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/enterobacteriales_odb9 .
+
+# copy small version of kraken2 database
+echo "copying Kraken2 files..."
+cp /ibex/scratch/projects/c2024/HTSworkshop2019_Genomics_data/*k2d .
+
