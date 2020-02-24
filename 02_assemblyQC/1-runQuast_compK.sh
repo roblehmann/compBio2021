d82a44b6f2d620fc9021bdf2d4d8c49e6434c9a6
lehmanr <robert.lehman@kaust.edu.sa>
2020-02-24 18:18:09 +0300

    fix path to assemblies

--- 02_assemblyQC/1-runQuast_compK.sh
+++ 02_assemblyQC/1-runQuast_compK.sh
@@ -16,11 +16,11 @@ set -vex
 
 # collect different assemblies for comparison
 
-ln -s ecoli_assembly-SRR2627019_40x/K21/final_contigs.fasta ecoli_1mb_40x_spades_k21.fasta
-ln -s ecoli_assembly-SRR2627019_40x/K33/final_contigs.fasta ecoli_1mb_40x_spades_k33.fasta
-ln -s ecoli_assembly-SRR2627019_40x/K55/final_contigs.fasta ecoli_1mb_40x_spades_k55.fasta
-ln -s ecoli_assembly-SRR2627019_40x/K77/final_contigs.fasta ecoli_1mb_40x_spades_k77.fasta
-ln -s ecoli_assembly-SRR2627019_40x/K99/final_contigs.fasta ecoli_1mb_40x_spades_k99.fasta
-ln -s ecoli_assembly-SRR2627019_40x/K127/final_contigs.fasta ecoli_1mb_40x_spades_k127.fasta
+ln -s ../01_assembly/ecoli_assembly-SRR2627019_40x/K21/final_contigs.fasta ecoli_1mb_40x_spades_k21.fasta
+ln -s ../01_assembly/ecoli_assembly-SRR2627019_40x/K33/final_contigs.fasta ecoli_1mb_40x_spades_k33.fasta
+ln -s ../01_assembly/ecoli_assembly-SRR2627019_40x/K55/final_contigs.fasta ecoli_1mb_40x_spades_k55.fasta
+ln -s ../01_assembly/ecoli_assembly-SRR2627019_40x/K77/final_contigs.fasta ecoli_1mb_40x_spades_k77.fasta
+ln -s ../01_assembly/ecoli_assembly-SRR2627019_40x/K99/final_contigs.fasta ecoli_1mb_40x_spades_k99.fasta
+ln -s ../01_assembly/ecoli_assembly-SRR2627019_40x/K127/final_contigs.fasta ecoli_1mb_40x_spades_k127.fasta
 
 quast.py ecoli_1mb_40x_spades_k*.fasta -t 5 -o kmer_comparison_quast > ecoli_quast.log
