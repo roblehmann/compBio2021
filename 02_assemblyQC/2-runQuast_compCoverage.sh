adf8026ea7b344a38cad2d12f5fdd5326b70de5c
lehmanr <robert.lehman@kaust.edu.sa>
2020-02-26 17:39:35 +0300

    fix changed parameter name for quast

--- 02_assemblyQC/2-runQuast_compCoverage.sh
+++ 02_assemblyQC/2-runQuast_compCoverage.sh
@@ -20,4 +20,4 @@ ln -s ../01_assembly/ecoli_assembly-SRR2627019_20x/scaffolds.fasta ecoli_1mb_20x
 ln -s ../01_assembly/ecoli_assembly-SRR2627019_40x/scaffolds.fasta ecoli_1mb_40x_spades.fasta
 ln -s ../01_assembly/ecoli_assembly-SRR2627019_80x/scaffolds.fasta ecoli_1mb_80x_spades.fasta
 
-quast.py ecoli_1mb_*x_spades.fasta -t 5 --scaffolds -o coverage_comparison_quast > ecoli_quast.log
+quast.py ecoli_1mb_*x_spades.fasta -t 5 -s -o coverage_comparison_quast > ecoli_quast.log
